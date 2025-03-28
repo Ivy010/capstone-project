8404c3c269bb5cd19e9a37f89230dc03b78a583c
Ivy010 <irmmwangi@gmail.com>
2025-03-28 16:21:13 +0300

    Setup the workout log page

--- fitness-tracker-app/src/components/WorkoutLog.jsx
+++ fitness-tracker-app/src/components/WorkoutLog.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const WorkoutLog = () => {
     return (
         <div>
-            <h2>WorkoutLog</h2>
+            <h2>Log a Workout</h2>  
+            <form>
+                <label for workoutName>Workout Name</label>
+                <input
+                 type="text"
+                 placeholder="Workout Name"
+                />
+
+                <label for duration> Duration</label>
+                <input
+                 type="number"
+                 placeholder="Duration (mins)"
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
